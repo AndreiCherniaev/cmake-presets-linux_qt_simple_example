288240d6c1c124e1d39c88f64f1dd71910ad557c
AndreiCherniaev <glscene2010@gmail.com>
2023-06-14 23:34:33 +0900

    init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+#include <signal.h>
+
+void SigInt_Handler(int n_signal){
+    printf("interrupted with code %u\n", n_signal); //Ctrl+C
+    QCoreApplication::instance()->quit();  //exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    signal(SIGINT, &SigInt_Handler);
+
+    int ret= QCoreApplication::exec();
+    return ret;
+}
